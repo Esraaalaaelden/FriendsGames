08c3cb29af12b4d55e86a92e5f826f154bd67896
ahmed ramadan <ahmed.ramadan20@outlook.com>
2023-09-09 23:46:06 +0300

    بسم الله

--- lib/start.dart
+++ lib/start.dart
@@ -7,27 +7,53 @@ class home extends StatefulWidget {
   @override
   State<home> createState() => _homeState();
 }
+
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home:
-      Scaffold(
-          body:Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF30CFD0),
-                    Color(0xFF330867),
-                    // Color(0xFF30CFD0),
-                    //Color(0xFF330867),
-                  ],
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                )
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF30CFD0),
+            Color(0xFF330867),
+            // Color(0xFF30CFD0),
+            //Color(0xFF330867),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              centerTitle: true,
+              toolbarHeight: 120,
+              pinned: true,
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
 
-          )
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF30CFD0),
+                      Color(0xFF330867),
+                      // Color(0xFF30CFD0),
+                      //Color(0xFF330867),
+                    ],
+                  ),
+                ),
+              ),
+              title: Image(
+                image: AssetImage("assets/Rectangle 2.png"),
+                height: 149,
+                width: 252,
+              ),
+            ),
+            SliverList(delegate: SliverChildListDelegate([
+
+            ]))
+          ],
+        ),
       ),
     );
   }
