659ba0169c0f6426fe5fb92a12d305b217585096
ahmed ramadan <ahmed.ramadan20@outlook.com>
2023-09-10 14:20:38 +0300

    try now

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   var teamOneController = TextEditingController();
   var teamTwoController = TextEditingController();
 
@@ -62,91 +63,115 @@ class _HomePageState extends State<HomePage> {
                 ),
               )
             ],
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Expanded(
-                        child: defaultTextFormField(
-                          hintText: 'Team one',
-                          controller: teamOneController,
-                          keyboardType: TextInputType.text,
+            body: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 100,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Expanded(
+                            child: defaultTextFormField(
+                              validator: (String? value) {
+                                if (value!.isEmpty) {
+                                  return 'you must type team one name!';
+                                }
+                                return null;
+                              },
+                              hintText: 'Team One',
+                              controller: teamOneController,
+                              keyboardType: TextInputType.text,
+                            ),
+                          ),
+                          const Image(
+                            image: AssetImage('assets/Ellipse 4.png'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Image(
+                      image: AssetImage('assets/Rectangle 6.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Image(
+                            image: AssetImage('assets/Ellipse 3.png'),
+                          ),
+                          Expanded(
+                            child: defaultTextFormField(
+                              validator: (String? value) {
+                                if (value!.isEmpty) {
+                                  return 'you must type team two name!';
+                                }
+                                return null;
+                              },
+                              hintText: 'Team Two',
+                              controller: teamTwoController,
+                              keyboardType: TextInputType.text,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      height: 55,
+                      decoration: BoxDecoration(
+
+                        //    boxShadow: <BoxShadow>[
+                        //   BoxShadow(
+                        //   color: Colors.black,
+                        //   blurRadius: 5.0,
+                        //   offset: Offset(0.0, 0.20),
+                        // ),
+                        //   ],
+
+                        gradient: LinearGradient(
+                          colors: [
+                            primaryColor,
+                            Colors.blue,
+                            secondaryColor,
+                          ],
                         ),
                       ),
-                      const Image(
-                        image: AssetImage('assets/Ellipse 4.png'),
-                      ),
-                    ],
-                  ),
-                ),
-                const Image(
-                  image: AssetImage('assets/Rectangle 6.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Image(
-                        image: AssetImage('assets/Ellipse 3.png'),
-                      ),
-                      Expanded(
-                        child: defaultTextFormField(
-                          hintText: 'Team two',
-                          controller: teamTwoController,
-                          keyboardType: TextInputType.text,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => const StartPage(),
+                              ),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                        ),
+                        child: const Text(
+                          'Start',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  height: 55,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
 
-                  decoration: const BoxDecoration(
-                  //    boxShadow: <BoxShadow>[
-                  //   BoxShadow(
-                  //   color: Colors.black,
-                  //   blurRadius: 5.0,
-                  //   offset: Offset(0.0, 0.20),
-                  // ),
-                  //   ],
-
-                      gradient: LinearGradient(
-                    colors: [primaryColor,Colors.blue,secondaryColor],
-                  ),),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                    ), child: const Text('Start',style: TextStyle(color: Colors.white,fontSize: 25),),
-                  ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                defaultButton(
-                  buttonColor: secondaryColor,
-                  onPress: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => const StartPage(),
-                      ),
-                    );
-                  },
-                  text: 'Start',
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  fontSize: 20,
-                )
-              ],
+              ),
             ),
           ),
         ),
